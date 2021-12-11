d2b57a39fc3ac9713a51d534c5774269a788f50e
jojoX94 <jose.augusteramarolahy@gmail.com>
2021-12-11 16:20:24 +0300

    Section company slider done

--- app/js/script.js
+++ app/js/script.js
@@ -22,3 +22,43 @@ btnHamburger.addEventListener("click", function () {
     });
   }
 });
+
+
+function Carousel(options){
+  var elem = this.elem = options.elem;
+  this.carousel = document.getElementById('carousel');
+  this.ulList = document.querySelector('ul');
+  this.liList = document.querySelector('li');
+  this.position = 0;
+  this.width = 263;
+
+  elem.getElementsByClassName('prev')[0].onclick = this.leftButtonClick.bind(this);
+  elem.getElementsByClassName('next')[0].onclick = this.rightButtonClick.bind(this);
+};
+
+
+Carousel.prototype.leftButtonClick = function(){
+  var self = this;
+  console.log(this.position + '!!');
+  if (this.position >= 0) {console.log('-1')} else {
+    console.log(this.position);
+    this.position = this.position + this.width;
+    console.log(this.position);
+    this.ulList.style.marginLeft = this.position + 'px'};
+  console.log(this.position);
+
+};
+
+Carousel.prototype.rightButtonClick = function(){
+  var self = this;
+  var numberOfItems = 8;
+
+  if (this.position <= -numberOfItems * 263) {console.log('-1')} else {
+    this.position = this.position - this.width;
+    this.ulList.style.marginLeft = this.position + 'px'
+    console.log(this.position)};
+};
+
+var carous = new Carousel({
+  elem: document.getElementById('carousel')
+});
